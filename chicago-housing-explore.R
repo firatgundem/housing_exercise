75154e16c462be0d590e39ec5d05a20676bfebab
Firat Gundem <firatgundem@gmail.com>
2018-11-06 13:19:39 -0600

    added codes

--- chicago-housing-explore.R
+++ chicago-housing-explore.R
@@ -54,3 +54,61 @@ left_join(properties_per_area, areas_key, by = "community_area_number") %>%
   select(community_area, community_area_number, properties = n)%>%
   arrange(desc(properties))
 
+units_per_area <- housing_tbl %>%
+  group_by(community_area_number)%>%
+  summarize(total_units = sum(units))
+
+left_join(units_per_area, areas_key, 
+          by = "community_area_number") %>%
+  select(community_area, community_area_number, total_units)%>%
+  arrange(desc(total_units))
+
+units_per_area
+
+ggplot()
+library(ggplot2)
+
+# Ctrl-Shift-R to insert a new section
+
+# Visualize data ----------------------------------------------------------
+
+ggplot(data = units_per_area, mapping = aes(x = total_units)) +
+  geom_histogram(binwidth = 100) + 
+  labs(x="Total Units By Community Area", y= "Count")
+
+
+#ggplot(units_per_area, aes(x = total_units)) +
+# geom_histogram(binwidth = 100) + 
+#labs(x="Total Units By Community Area", y= "Count")
+
+#make a histogram for the number of properties per area
+
+
+ggplot(properties_per_area, aes(x = n)) +
+  geom_histogram() + 
+  labs(x="Total Properties By Community Area", y= "Count")
+
+# alt - For assignment you can use "alt -" to be fast
+
+library(leaflet)
+
+# Make a leaflet map ------------------------------------------------------
+
+# ctrl shift m for pipe
+
+leaflet() %>% 
+  addTiles() %>% 
+  addCircles(lng = housing_tbl$longitude, lat = housing_tbl$latitude)
+
+leaflet() %>% 
+  addProviderTiles(providers$CartoDB) %>% 
+  addCircles(lng = housing_tbl$longitude, lat = housing_tbl$latitude)
+
+leaflet() %>% 
+  addProviderTiles(providers$CartoDB) %>% 
+  addCircles(lng = housing_tbl$longitude, 
+             lat = housing_tbl$latitude, 
+             popup = housing_tbl$property_name,
+             radius = housing_tbl$units)
+
+library(sf)
